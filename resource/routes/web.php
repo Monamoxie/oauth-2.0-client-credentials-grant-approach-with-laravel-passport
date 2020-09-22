5c1dd1e84d5dc256600ca9c33468ef300d57b0b9
Mona Moxie <moxie4lyf@gmail.com>
2020-09-22 12:48:01 +0100

    resource: uodate dashboard

--- resource/routes/web.php
+++ resource/routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])
+->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])
+->get('/posts/create', [App\Http\Controllers\PostsController::class, 'store']);
