d7027ca82a3b6058b9a439eeb3a477d4e0a27046
Mona Moxie <moxie4lyf@gmail.com>
2020-09-22 13:27:54 +0100

    resource: update dashboard contents

--- resource/resources/views/auth/register.blade.php
+++ resource/resources/views/auth/register.blade.php
@@ -4,23 +4,15 @@
             <x-jet-authentication-card-logo />
         </x-slot>
 
+         <h5 style="text-align:center; padding: 10px; font-weight: bold"> PLEASE REGISTER ON RESOURCE </h5>
         <x-jet-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div class="mt-4">
-                <label class="block font-medium text-sm text-gray-700">
-                    Registering as
-                </label>
-                <select class="block mt-1 w-full block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" 
-                    name="user_type" required :value="old('user_type')">
-                    <option value="user">User</option>
-                    <option value="client">Client</option>
-                </select>
-            </div>
+          
 
-            <div>
+            <div class="mt-4">
                 <x-jet-label value="{{ __('Name') }}" />
                 <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
@@ -38,11 +30,11 @@
             <div class="mt-4">
                 <x-jet-label value="{{ __('Confirm Password') }}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+            </div> 
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Already registered on resource?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
